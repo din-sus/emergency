eb2eacaf1bce9727ba8fa5617c59da74755fd596
din-sus <tuhtamatovdinmuhammad2@gmail.com>
2024-08-17 09:50:34 +0500

    graphql server done

---
+++ src/graphql/index.js
@@ -0,0 +1,36 @@
+const {WebSocketServer} = require ('ws');
+const { ApolloServerPluginDrainHttpServer } = require ('@apollo/server/plugin/drainHttpServer');
+const {useServer} = require ('graphql-ws/lib/use/ws');
+const schema = require ('./schema');
+const {ApolloServer} = require ('@apollo/server');
+
+
+const buildGraphqlServer = (httpServer) => {
+    let wsServer = new WebSocketServer({
+        server: httpServer,
+        path: '/'
+    })
+
+    let serverCleanup = useServer({schema}, wsServer)
+
+    return new ApolloServer({
+        schema,
+        plugins: [
+            ApolloServerPluginDrainHttpServer({httpServer}),
+            {
+                async serverWillStart() {
+                  return {
+                    async drainServer() {
+                      await serverCleanup.dispose();
+                    },
+                  };
+                },
+            },
+        ],
+    })
+}
+
+
+
+
+// module.exports = buildGraphqlServer -> has to be exported to index.js which is located in graphql file
---
+++ src/graphql/pubsub.js
@@ -0,0 +1,5 @@
+// pubsub has to be installed
+
+const {PubSub} = require('pubsub')
+
+// module.exports = new PubSub() -> has to be written
